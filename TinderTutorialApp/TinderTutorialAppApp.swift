426f10ddc2a25daae349c3a416328697804c84a2
Aram <aramsaribekyan1@gmail.com>
2024-03-18 12:58:01 +0400

    Initial Commit

---
+++ TinderTutorialApp/TinderTutorialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderTutorialAppApp.swift
+//  TinderTutorialApp
+//
+//  Created by Aram on 18.03.24.
+//
+
+import SwiftUI
+
+@main
+struct TinderTutorialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
